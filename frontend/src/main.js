5c2c1052cae102f9ab6e06008d29ff3889c570c0
ItsMaxDev <112982786+ItsMaxDev@users.noreply.github.com>
2024-04-01 18:42:48 +0200

    Import Pinia and load user login state

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,11 +1,22 @@
 import './style.css';
 
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import { useAccountStore } from './stores/AccountStore';
 import App from './App.vue'
 import router from './router'
 
+const pinia = createPinia()
 const app = createApp(App)
 
 app.use(router)
+app.use(pinia)
 
-app.mount('#app')
+// Load the user's login state
+const accountStore = useAccountStore()
+accountStore.load().then(() => {
+    app.mount('#app')
+}).catch((err) => {
+    console.error(err)
+    app.mount('#app')
+});
