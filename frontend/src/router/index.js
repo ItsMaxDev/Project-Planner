c0fd5c6534580ec93e69fcc059d36f8c8b322f6c
ItsMaxDev <112982786+ItsMaxDev@users.noreply.github.com>
2024-04-09 21:47:25 +0200

    Add new route for profile page

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -4,6 +4,7 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     { path: '/', name: 'home', component: () => import('../views/Home.vue') },
+    { path: '/profile', name: 'profile', component: () => import('../views/Profile.vue') },
     { path: '/projects/add', name: 'addproject', component: () => import('../views/projects/AddProject.vue') },
     { path: '/projects/edit/:id', name: 'editproject', component: () => import('../views/projects/EditProject.vue'), props: true }
   ]
