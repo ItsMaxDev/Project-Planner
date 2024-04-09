2cb3f71f4ba2d305182698b0f0a5a3f61ec1ab23
ItsMaxDev <112982786+ItsMaxDev@users.noreply.github.com>
2024-04-09 21:48:19 +0200

    Turn navbar into component

---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { useAccountStore } from '@/stores/AccountStore';
+
+const accountStore = useAccountStore();
+</script>
+
+<template>
+    <div v-if="accountStore.isLoggedIn">
+        <div class="flex justify-end">
+            <RouterLink :to="{ name: 'profile' }" class="mt-5 me-5 flex items-center font-bold gap-1.5">
+                <span class="material-symbols-outlined">
+                    account_circle
+                </span>
+                <p>{{ accountStore.user.username }}</p>
+            </RouterLink>
+        </div>
+    </div>
+</template>
