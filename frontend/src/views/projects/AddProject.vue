df01dbb506ea09aef3301762bc9039d9815b3677
ItsMaxDev <112982786+ItsMaxDev@users.noreply.github.com>
2024-04-09 21:46:02 +0200

    Add more top margin

--- frontend/src/views/projects/AddProject.vue
+++ frontend/src/views/projects/AddProject.vue
@@ -21,7 +21,7 @@ async function createProject() {
 </script>
 
 <template>
-    <div class="flex flex-col items-center mt-20">
+    <div class="flex flex-col items-center mt-40">
         <div v-if="error" role="alert" class="alert alert-error w-1/4 mb-2 rounded-lg">
             <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
             <span>{{ error }}</span>
