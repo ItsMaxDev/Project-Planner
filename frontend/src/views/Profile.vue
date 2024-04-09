76465c9f927143fcf37c89811e770c419d403559
ItsMaxDev <112982786+ItsMaxDev@users.noreply.github.com>
2024-04-09 21:47:47 +0200

    Add Profile Page

---
+++ frontend/src/views/Profile.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { useAccountStore } from '@/stores/AccountStore';
+
+const accountStore = useAccountStore();
+</script>
+
+<template>
+    <div class="flex flex-col items-center mt-40">
+        <button class="btn btn-neutral w-1/6" @click="$router.push({ name: 'home' })"><span class="material-icons">arrow_back</span>Go back to projects</button>
+        <div class="w-1/6 mt-2">
+            <div class="bg-base-300 p-5 rounded-lg shadow-lg">
+                <h1 class="text-center">Profile</h1>
+                <div class="mt-4">
+                    <label class="font-bold">ID:</label>
+                    <span class="ml-2">{{ accountStore.user.id }}</span>
+                </div>
+                <div class="mt-4">
+                    <label class="font-bold">Username:</label>
+                    <span class="ml-2">{{ accountStore.user.username }}</span>
+                </div>
+                <div class="mt-4">
+                    <label class="font-bold">Email:</label>
+                    <span class="ml-2">{{ accountStore.user.email }}</span>
+                </div>
+                <div class="mt-4">
+                    <label class="font-bold">Is Admin:</label>
+                    <span class="ml-2" :class="accountStore.isAdmin ? 'badge badge-success' : 'badge badge-error'">{{ accountStore.isAdmin ? 'Yes' : 'No' }}</span>
+                </div>
+                <div class="mt-4">
+                    <button @click="accountStore.logout" class="btn btn-error w-full">Logout</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
