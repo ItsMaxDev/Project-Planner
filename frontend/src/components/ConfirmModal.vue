a48eb5ee3ed35996d4177b03aba40eff8849e2bc
ItsMaxDev <112982786+ItsMaxDev@users.noreply.github.com>
2024-04-14 13:21:12 +0200

    Improve ConfirmModal style

--- frontend/src/components/ConfirmModal.vue
+++ frontend/src/components/ConfirmModal.vue
@@ -13,11 +13,11 @@ defineEmits(['yes', 'no'])
     <teleport to="body">
         <div class="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 p-5">
             <div class="flex justify-center">
-                <div class="bg-white p-4 rounded shadow text-black w-fit">
+                <div class="bg-base-200 p-4 rounded-lg shadow w-fit">
                     <h2 class="text-lg font-bold mb-2">{{ question }}</h2>
                     <div class="flex justify-end">
-                        <button @click="$emit('no')" class="px-4 py-2 mr-2 bg-gray-200 rounded">No</button>
-                        <button @click="$emit('yes')" class="px-4 py-2 bg-red-500 text-white rounded">Yes</button>
+                        <button @click="$emit('no')" class="px-4 py-2 mr-2 btn btn-neutral">No</button>
+                        <button @click="$emit('yes')" class="px-4 py-2 btn btn-error">Yes</button>
                     </div>
                 </div>
             </div>
