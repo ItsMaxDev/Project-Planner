f0bd50ddc1de696a4fa3f03956a3f38783cdb515
ItsMaxDev <112982786+ItsMaxDev@users.noreply.github.com>
2024-04-14 12:48:21 +0200

    Make add/edit project mobile friendly

--- frontend/src/views/projects/EditProject.vue
+++ frontend/src/views/projects/EditProject.vue
@@ -37,28 +37,30 @@ const editProject = async () => {
 </script>
 
 <template>
-    <div class="flex flex-col items-center mt-40">
-        <Alert v-if="error" :message="error" type="error" class="w-1/4 mb-2 rounded-lg" />
-        <button class="btn btn-neutral w-1/4" @click="$router.push({ name: 'home' })"><span class="material-icons">arrow_back</span>Go back to projects</button>
-        <form v-if="project" @submit.prevent="editProject" class="w-1/4 mt-2">
-            <div class="bg-base-300 p-5 rounded-lg shadow-lg">
-                <h1 class="text-center">Edit Project</h1>
-                <div class="mt-4 flex flex-col">
-                    <label class="mb-1">Name</label>
-                    <input v-model="project.name" type="text" placeholder="My project" maxlength="32" class="input input-bordered" required>
+    <div class="flex items-center justify-center h-screen">
+        <div class="w-full max-w-md grid p-5">
+            <Alert v-if="error" :message="error" type="error" class="mb-2 rounded-lg" />
+            <button class="btn btn-neutral" @click="$router.push({ name: 'home' })"><span class="material-icons">arrow_back</span>Go back to projects</button>
+            <form v-if="project" @submit.prevent="editProject" class="mt-2">
+                <div class="bg-base-300 p-5 rounded-lg shadow-lg">
+                    <h1 class="text-center">Edit Project</h1>
+                    <div class="mt-4 flex flex-col">
+                        <label class="mb-1">Name</label>
+                        <input v-model="project.name" type="text" placeholder="My project" maxlength="32" class="input input-bordered" required>
+                    </div>
+                    <div class="mt-4 flex flex-col">
+                        <label class="mb-1">Description</label>
+                        <textarea v-model="project.description" rows="5" maxlength="65535" class="textarea textarea-bordered"></textarea>
+                    </div>
+                    <div class="mt-4 flex flex-col">
+                        <label class="mb-1">Due Date</label>
+                        <input v-model="project.due_date" type="datetime-local" class="input input-bordered">
+                    </div>
+                    <div class="mt-4 flex justify-end">
+                        <button class="btn btn-primary">Update</button>
+                    </div>
                 </div>
-                <div class="mt-4 flex flex-col">
-                    <label class="mb-1">Description</label>
-                    <textarea v-model="project.description" rows="5" maxlength="65535" class="textarea textarea-bordered"></textarea>
-                </div>
-                <div class="mt-4 flex flex-col">
-                    <label class="mb-1">Due Date</label>
-                    <input v-model="project.due_date" type="datetime-local" class="input input-bordered">
-                </div>
-                <div class="mt-4 flex justify-end">
-                    <button class="btn btn-primary">Update</button>
-                </div>
-            </div>
-        </form>
+            </form>
+        </div>
     </div>
 </template>
