6a4088b8cd5be064c081a0ebdd79a5efe0f09a1c
ItsMaxDev <112982786+ItsMaxDev@users.noreply.github.com>
2024-04-14 12:26:54 +0200

    Make login/signup mobile friendly

--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -30,49 +30,51 @@ const register = async () => {
 </script>
 
 <template>
-    <div class="flex flex-col items-center justify-center h-screen">
-        <Alert v-if="error" :message="error" type="error" class="w-2/12 mb-2" />
-        <form v-if="!signup" @submit.prevent="login" class="w-2/12">
-            <div class="bg-base-300 p-5 rounded-lg shadow-lg">
-                <h1 class="text-center">Login</h1>
-                <div class="mt-4 flex flex-col">
-                    <label class="mb-1">Username</label>
-                    <input v-model="username" type="text" maxlength="16" class="input input-bordered" required>
+    <div class="flex items-center justify-center h-screen">
+        <div class="flex flex-col">
+            <Alert v-if="error" :message="error" type="error" class="mb-2 rounded-lg" />
+            <form v-if="!signup" @submit.prevent="login" class="w-full">
+                <div class="bg-base-300 p-5 rounded-lg shadow-lg">
+                    <h1 class="text-center">Login</h1>
+                    <div class="mt-4 flex flex-col">
+                        <label class="mb-1">Username</label>
+                        <input v-model="username" type="text" maxlength="16" class="input input-bordered" required>
+                    </div>
+                    <div class="mt-4 flex flex-col">
+                        <label class="mb-1">Password</label>
+                        <input v-model="password" type="password" class="input input-bordered" required>
+                    </div>
+                    <div class="mt-4 flex justify-end">
+                        <button class="btn btn-primary w-full">Login</button>
+                    </div>
+                    <div class="mt-6 flex justify-center">
+                        <p>Don't have an account? <a @click="signup = true" class="link link-info">Sign up</a></p>
+                    </div>
                 </div>
-                <div class="mt-4 flex flex-col">
-                    <label class="mb-1">Password</label>
-                    <input v-model="password" type="password" class="input input-bordered" required>
+            </form>
+            <form v-else @submit.prevent="register" class="w-full">
+                <div class="bg-base-300 p-5 rounded-lg shadow-lg">
+                    <h1 class="text-center">Sign up</h1>
+                    <div class="mt-4 flex flex-col">
+                        <label class="mb-1">Username</label>
+                        <input v-model="username" type="text" maxlength="16" class="input input-bordered" required>
+                    </div>
+                    <div class="mt-4 flex flex-col">
+                        <label class="mb-1">Email</label>
+                        <input v-model="email" type="email" class="input input-bordered" required>
+                    </div>
+                    <div class="mt-4 flex flex-col">
+                        <label class="mb-1">Password</label>
+                        <input v-model="password" type="password" class="input input-bordered" required>
+                    </div>
+                    <div class="mt-4 flex justify-end">
+                        <button class="btn btn-primary w-full">Sign up</button>
+                    </div>
+                    <div class="mt-6 flex justify-center">
+                        <p>Already have an account? <a @click="signup = false" class="link link-info">Login</a></p>
+                    </div>
                 </div>
-                <div class="mt-4 flex justify-end">
-                    <button class="btn btn-primary w-full">Login</button>
-                </div>
-                <div class="mt-6 flex justify-center">
-                    <p>Don't have an account? <a @click="signup = true" class="link link-info">Sign up</a></p>
-                </div>
-            </div>
-        </form>
-        <form v-else @submit.prevent="register" class="w-2/12">
-            <div class="bg-base-300 p-5 rounded-lg shadow-lg">
-                <h1 class="text-center">Sign up</h1>
-                <div class="mt-4 flex flex-col">
-                    <label class="mb-1">Username</label>
-                    <input v-model="username" type="text" maxlength="16" class="input input-bordered" required>
-                </div>
-                <div class="mt-4 flex flex-col">
-                    <label class="mb-1">Email</label>
-                    <input v-model="email" type="email" class="input input-bordered" required>
-                </div>
-                <div class="mt-4 flex flex-col">
-                    <label class="mb-1">Password</label>
-                    <input v-model="password" type="password" class="input input-bordered" required>
-                </div>
-                <div class="mt-4 flex justify-end">
-                    <button class="btn btn-primary w-full">Sign up</button>
-                </div>
-                <div class="mt-6 flex justify-center">
-                    <p>Already have an account? <a @click="signup = false" class="link link-info">Login</a></p>
-                </div>
-            </div>
-        </form>
+            </form>
+        </div>
     </div>
 </template>
